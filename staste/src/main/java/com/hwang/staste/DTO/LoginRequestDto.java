c737fb6a64fbc8dce8c47937c7fe4b25104bdc20
cksghkd123 <cksghkd1234@naver.com>
2023-03-14 00:45:35 +0900

    로그인 구현중

---
+++ staste/src/main/java/com/hwang/staste/DTO/LoginRequestDto.java
@@ -0,0 +1,9 @@
+package com.hwang.staste.DTO;
+
+import lombok.Data;
+
+@Data
+public class LoginRequestDto {
+    private String username;
+    private String password;
+}
--- staste/src/main/java/com/hwang/staste/config/jwt/JwtTokenProvider.java
+++ staste/src/main/java/com/hwang/staste/config/jwt/JwtTokenProvider.java
@@ -1,12 +1,23 @@
 package com.hwang.staste.config.jwt;
 
 import com.auth0.jwt.JWT;
-import com.hwang.staste.DTO.JwtToken;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.hwang.staste.config.auth.PrincipalDetails;
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
 import java.util.stream.Collectors;
 
 @Component
@@ -14,45 +25,53 @@ import java.util.stream.Collectors;
 public class JwtTokenProvider {
 
     public String createToken(Authentication authentication) {
-        String authorities = authentication.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.joining(" "));
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
 
         String accessToken = JWT.create()
-                .withClaim()
+                .withSubject(authentication.getName())
+                .withClaim("id", principalDetails.getUser().getId())
+                .withClaim("username", principalDetails.getUser().getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + JwtProperties.EXPIRATION_TIME))
+                .sign(Algorithm.HMAC256(JwtProperties.SECRET));
 
-        String refreshToken;
+        String refreshToken = JWT.create()
+                .withExpiresAt(new Date(System.currentTimeMillis() + JwtProperties.EXPIRATION_TIME * 36))
+                .sign(Algorithm.HMAC256(JwtProperties.SECRET));
 
 
-        return JwtToken.builder()
-                .grantType("Bearer")
-                .accessToken(accessToken)
-                .refreshToken(refreshToken)
-                .build();
+        return JWT.create()
+                .withClaim("token_type", "Bearer")
+                .withClaim("access_token", accessToken)
+                .withClaim("refresh_token", refreshToken)
+                .sign(Algorithm.none());
+    }
+
+    public Authentication getAuthentication(String accessToken) {
+        DecodedJWT decodedJWT = JWT.decode(accessToken);
+
+        if (!decodedJWT.getClaim("auth").isNull()) {
+            Collection<? extends GrantedAuthority> authorities = Arrays.stream(decodedJWT.getClaim("auth").asArray(String.class))
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+
+            PrincipalDetails principalDetails = new PrincipalDetails(decodedJWT.getSubject(), "", authorities);
+            return new UsernamePasswordAuthenticationToken(principalDetails, "", authorities);
+        }
+        return null;
+    }
+    public DecodedJWT decodeToken(String token) throws JWTDecodeException {
+        return JWT.decode(token);
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            JWT.require(Algorithm.HMAC256(JwtProperties.SECRET)).build().verify(token);
+            return true;
+        } catch (JWTVerificationException e) {
+            System.out.println(e);
+        }
+        return false;
     }
 
 }
 
-//    Strint jwtToken = Jwts.builder().addClaims()
-//            .Map.of(
-//                    "exp",
-//                    "key1", "value1",
-//                    "key2", "value2", ...
-//        )
-//        .signWith(SignatureAlgorithm.HS256, "JWT_SECRET_KEY")
-//        .compact();;
-//
-//        Jws<Claims> jwt = Jwts.parser()
-//        .setSigningKey("JWT_SECRET_KEY")
-//        .parseClaimsJws(jwtToken);
-//
-//    String jwtToken = JWT.create()
-//            .withSubject("subject"
-//                    .withExpiresAt(new Date(System.currentTimeMillis() + ... )
-//        .withClaim("key2", "value2")
-//        .sign(Algorithm.HMAC256("JWT_SECRET_KEY"));
-//        Payload 파싱방법 (토큰 디코딩)
-//
-//        DecodedJWT jwt = JWT.require(Algorithm.HMAC256("JWT_SECRET_KEY"))
-//        .build()
-//        .verify(jwtToken);
